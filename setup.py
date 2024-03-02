fc9daf7fc76ee2a0a974e734a00d76be46cb412a
Sadam·Sadik <1903249375@qq.com>
2024-03-02 09:24:34 +0800

    小调整

--- setup.py
+++ setup.py
@@ -29,23 +29,6 @@ setup(
     packages=["saer"],
     include_package_data=True,
     long_description=long_description,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
-        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
-        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator"
-    ],
     keywords=['saer', 'openapi', 'api', 'sdk', 'python-sdk', "api.isaerdata.com"],
     entry_points={
         'console_scripts': [
